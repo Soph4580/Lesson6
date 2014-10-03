2703e0ed7a75116b8ca69d6253e0d3ceb44aafc1
soph4580 <soph4580@ISS147D8-26.scdsb.pri>
2014-10-03 09:03:24 -0400

    Oct 3 2nd

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,39 @@
 public class Employee {
     
+    private String name;
+    private int type;
+    private double rate;
+    private int hours;
+    
+    public Employee(){
+        name="";
+        type=0;
+        rate=0;
+        hours=0;
+        
+    }
+    
+    public String getTypeRules(){
+        return"1 or 2";
+    }
+    
+    public String getRateRules(){
+        return"noblank";
+    }
+    
+    public String getHoursRules(){
+        return "between 6.75 and 30.50, inclusive";
+    }
+    
+    public boolean setName(String nm){
+        boolean blank = (nm.equals(""));
+        if(blank)
+         return false;
+        else{
+            name=nm;
+            return true;
+        }
+    }
+    
+    
 }
