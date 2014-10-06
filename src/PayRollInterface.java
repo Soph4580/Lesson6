7f6da856cfe8cebe6c680cf237850e146e2bab9a
soph4580 <soph4580@ISS147D8-26.scdsb.pri>
2014-10-06 08:48:23 -0400

    Oct 6

---
+++ src/PayRollInterface.java
@@ -0,0 +1,55 @@
+import TerminalIO.*;
+
+public class PayRollInterface{
+    
+public static void main(String[] args){
+
+KeyboardReader kr = new KeyboardReader();
+
+	Employee empl; //The employee
+	String name; //The name
+	int type; //The type
+	double rate; //Hourly rate
+	int hours; //The hours worked
+	String prompt; //The user prompt
+	empl= new Employee();
+        
+while(true){
+    System.out.println ("Enter employee data");
+    name = kr.readLine(" Name (or <enter> to quit):" );
+    if(! empl.setName(name)) break;
+}        
+
+while(true){ //Type the name in and break if blank
+
+	prompt = "Type"+ (empl.getTypeRules());
+	type= kr.readInt(prompt);
+	if(empl.setType(type)) break;
+	
+}
+
+while(true){ //Get the type until it is valid
+
+	prompt = "Hourly rate"+ (empl.getRateRules());
+	type= kr.readDouble(prompt);
+	if(empl.setRate(rate)) break; 
+
+}
+
+while(true){
+
+	prompt= ("Hours"+ (empl.getHourRules());
+	type= kr.readInt(prompt);
+	if(empl.setHours(hours)) break;
+}
+
+System.out.println("The weekly pay for ");
+System.out.println(empl.getName());
+System.out.println(" is $" + empl.getPay());
+
+}
+
+}
+
+
+
